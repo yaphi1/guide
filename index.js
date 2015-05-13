3b65e40a81acf16268b92a25990e73cc8d6e4070
yaphi1 <yaphi1@Yaphis-Air.home>
2015-05-13 07:42:17 -0400

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function(req,res){
 
 
 
-//var port = Number(Process.env.port || 1337);
+//var port = Number(process.env.PORT || 1337);
 var port = 1337;
 
 http.listen(port, function(){
@@ -35,7 +35,7 @@ var my_data = {};
 
 
 // the file where the data will be kept
-var data_store = 'data2.json';
+var data_store = 'data.json';
 
 function addData(key, value, callback){
 	// try to read this file
@@ -49,7 +49,7 @@ function addData(key, value, callback){
 
 			fs.writeFile(data_store, JSON.stringify(my_data), function(err){
 				if(err){throw err;}
-				console.log('data2.json saved!');
+				console.log(data_store+' saved!');
 
 				if(typeof(callback) == 'function'){ callback(); } // optional function to run after data added
 			});
@@ -59,28 +59,20 @@ function addData(key, value, callback){
 
 		// if there is a file parse its data, and then add to it
 		var obj = JSON.parse(data);
-		obj[key] = value; // just adding to the data
+		obj[key] = value; // add the data in the requested key
 
 		console.log('added the following data: '+key+':'+value);
-
-		// some logs just to see what's going on
-		// console.log('hey, the file says: '+data);
-		// console.log('we can also get individual pieces like this: '+obj.a.sub1);
-
 
 		// write the new version of the file
 		fs.writeFile(data_store, JSON.stringify(obj), function(err){
 			if(err){throw err;}
-			console.log('data2.json updated with added data');
+			console.log(data_store+' updated with added data');
 
 			if(typeof(callback) == 'function'){ callback(); } // optional function to run after data added
 		});
 	});
 }
-/*addData('newkey2','newvalue', function(){
-	console.log('add data callback happened');
-	getData('newkey2', function(){console.log('good get data callback happened');});
-});*/
+
 
 
 
@@ -105,7 +97,7 @@ function removeData(key, callback){
 		// write the new version of the file
 		fs.writeFile(data_store, JSON.stringify(obj), function(err){
 			if(err){throw err;}
-			console.log('data2.json updated with deletion');
+			console.log(data_store+' updated with deletion');
 
 			if(typeof(callback) == 'function'){ callback(); } // optional function to run after data removed
 		});
@@ -114,30 +106,6 @@ function removeData(key, callback){
 
 
 
-function getData(socket, key, callback){
-	// note that socket should only ever say socket
-	// this is so that socket will be defined inside the readFile call
-
-	
-	// try to read this file
-	fs.readFile(data_store, 'utf-8', function(err, data){
-		
-		// if there's no file, log that and move along
-		if(err){
-			console.log('no file found, so nothing to delete');
-			return err;
-		}
-
-		// if there is a file parse its data, and then get the item from it
-		var obj = JSON.parse(data);
-		var result = obj[key];
-
-		console.log('retrieved '+result);
-		socket.emit('got_profile', [key,result]); // send out the data
-
-		if(typeof(callback) == 'function'){ callback(); } // optional function to run after data retrieved
-	});
-}
 
 
 
@@ -159,14 +127,71 @@ io.on('connection', function(socket){
 	console.log('a user connected');
 
 
-	socket.on('create_profile',function(data){
-		addData(data[0], data[1]);
+	socket.on('new_note',function(data){
+		addData(data[0], data[1], function(){
+			updateAllData();
+		});
 	});
 
 
-	socket.on('request_profile',function(username){
+	socket.on('save', function(data){
+		for(var key in data){
+			addData(key, data[key]);
+		}
+	});
+
+	socket.on('delete_note', function(key){
+		removeData(key);
+	});
+
+
+	/*socket.on('request_profile',function(username){
 		getData(socket, username);
-	});
+	});*/
+
+
+	function updateAllData(callback){
+	
+		// try to read this file
+		fs.readFile(data_store, 'utf-8', function(err, data){
+			
+			// if there's no file, log that and move along
+			if(err){ console.log('no file found'); return err; }
+
+			// if there is a file parse its data, and then get the item from it
+			var obj = JSON.parse(data);
+
+			console.log('retrieved all data');
+			socket.emit('update_all_data', obj); // send out the data
+
+			if(typeof(callback) == 'function'){ callback(); } // optional function to run after data retrieved
+		});
+	}
+	updateAllData();
+
+
+
+
+
+	function getData(socket, key, callback){
+
+		// try to read this file
+		fs.readFile(data_store, 'utf-8', function(err, data){
+			
+			// if there's no file, log that and move along
+			if(err){ console.log('no file found'); return err; }
+
+			// if there is a file parse its data, and then get the item from it
+			var obj = JSON.parse(data);
+			var result = obj[key];
+
+			console.log('retrieved '+result);
+			socket.emit('got_profile', [key,result]); // send out the data
+
+			if(typeof(callback) == 'function'){ callback(); } // optional function to run after data retrieved
+		});
+	}
+
 });
 
 
